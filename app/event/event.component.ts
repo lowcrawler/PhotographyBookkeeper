e8543f2e830b90ea00ffd3831379a15ec78311b1
lowcrawler <joe.federer@gmail.com>
2017-01-23 20:54:41 -0600

    Basic PoC for Event Classes

--- app/event/event.component.ts
+++ app/event/event.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit} from '@angular/core';
 import { Event, Print, Photoshoot, Wedding, Newborn } from '../classes/events';
 
 @Component({
@@ -8,7 +8,19 @@ import { Event, Print, Photoshoot, Wedding, Newborn } from '../classes/events';
 })
 
 
-export class EventComponent  {
+export class EventComponent  implements OnInit {
 @Input()
 	eventName : string;
+
+	ngOnInit():void {
+		let test_event:Event = new Event(123, "test event", "test notes", "test_eventtype");
+		let test_newborn:Event = new Newborn(456, "test newborn event", "test newborn notes", "test_type_newborn", "test datetime", 2.5, "1/22/1981", "newborn", "testEstimateDate");
+	    console.log("EventComponent ngOnInit");
+		console.log("Event: ");
+		console.log(test_event);
+		console.log("Newborn");
+		console.log(test_newborn)
+	  }
+
+
  }
